863cb945b9e450e8ec605f0e352929a41070c5bf
Asha Kripalani <119749754+ashakrip@users.noreply.github.com>
2023-02-19 14:05:55 +0530

    Update HardContract.sol

--- HardContract.sol
+++ HardContract.sol
@@ -1,48 +1,27 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.0;
+pragma solidity =0.7.6;
 
-import "./interfaces/IUniswapV3PoolDeployer.sol";
+/// @title Prevents delegatecall to a contract
+/// @notice Base contract that provides a modifier for preventing delegatecall to methods in a child contract
+abstract contract NoDelegateCall {
+    /// @dev The original address of this contract
+    address private immutable original;
 
-import "./UniswapV3Pool.sol";
-
-contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
-    struct Parameters {
-        address factory;
-        address token0;
-        address token1;
-        uint24 fee;
-        int24 tickSpacing;
+    constructor() {
+        // Immutables are computed in the init code of the contract, and then inlined into the deployed bytecode.
+        // In other words, this variable won't change when it's checked at runtime.
+        original = address(this);
     }
 
-    /// @inheritdoc IUniswapV3PoolDeployer
-    Parameters public override parameters;
+    /// @dev Private method is used instead of inlining into modifier because modifiers are copied into each method,
+    ///     and the use of immutable means the address bytes are copied in every place the modifier is used.
+    function checkNotDelegateCall() private view {
+        require(address(this) == original);
+    }
 
-    /// @dev Deploys a pool with the given parameters by transiently setting the parameters storage slot and then
-    /// clearing it after deploying the pool.
-    /// @param factory The contract address of the Uniswap V3 factory
-    /// @param token0 The first token of the pool by address sort order
-    /// @param token1 The second token of the pool by address sort order
-    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// @param tickSpacing The spacing between usable ticks
-    function deploy(
-        address factory,
-        address token0,
-        address token1,
-        uint24 fee,
-        int24 tickSpacing
-    ) internal returns (address pool) {
-        parameters = Parameters({
-            factory: factory,
-            token0: token0,
-            token1: token1,
-            fee: fee,
-            tickSpacing: tickSpacing
-        });
-        pool = address(
-            new UniswapV3Pool{
-                salt: keccak256(abi.encode(token0, token1, fee))
-            }()
-        );
-        delete parameters;
+    /// @notice Prevents delegatecall into the modified method
+    modifier noDelegateCall() {
+        checkNotDelegateCall();
+        _;
     }
 }
