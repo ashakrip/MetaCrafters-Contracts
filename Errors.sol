5f5956b08823d0ad0807adc2159c8bbb40c549a4
Asha Kripalani <119749754+ashakrip@users.noreply.github.com>
2023-02-21 14:30:02 +0530

    Add files via upload

---
+++ Errors.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: UNLICENSED;
+pragma solidity ^0.8.13;
+
+contract Error {
+    function testRequire(uint256 _amt) public pure {
+        require(_amt > 100, "Minimum input of 100 required");
+    }
+
+    function testRevert(uint256 _i) public pure {
+        if (_amt <= 10) {
+            revert("Value must be 100 or more");
+        }
+    }
+
+    uint256 public num;
+
+    function testAssert() public view {
+        assert(num == 0);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+    }
+}
